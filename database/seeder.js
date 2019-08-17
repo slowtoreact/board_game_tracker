1106d00a8479770f44023bbb1b03bdd2fde13052
Daniel Schewe <danielschewe@Daniels-MacBook-Pro.local>
2019-08-17 12:06:17 -0600

    modified seeding function, csv file can now be imported into DB

--- database/seeder.js
+++ database/seeder.js
@@ -1,27 +1,44 @@
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
-let gameNames = ["Risk", "Life", "Connect-4", "Mouse-Trap", "Uno", "Sorry", "Coux", "Backgammon", "Cootie", "Don't Break the Ice", "Outlaw", "Wizard Battle", "Exploding Kittens", "Sushi Go", "Snakes and Ladders", "Payday", "13 Dead End Drive", "Cards Against Humanity", "What Do You Meme", "Scrabble", "Boggle", "Pictionary", "Balderdash", "Taboo", "Catch Phrase", "Lords of Waterdeep", "Settlers of Catan", "Twister"];
+var knex = require('knex')({
+    client: 'pg',
+    version: '7.2',
+    connection: {
+      host : '127.0.0.1',
+      user : 'your_database_user',
+      password : 'your_database_password',
+      database : 'myapp_test'
+    }
+  });
+
+  var pg = require('knex')({
+    client: 'pg',
+    connection: process.env.PG_CONNECTION_STRING,
+    searchPath: ['knex', 'public']
+  });
+
+let gameNames = ["Risk", "Life", "Connect-4", "Mouse-Trap", "Uno", "Sorry", "Coux", "Backgammon", "Cootie", "Don't Break the Ice", "Outlaw", "Wizard Battle", "Exploding Kittens", "Sushi Go", "Snakes and Ladders", "Payday", "13 Dead End Drive", "Cards Against Humanity", "What Do You Meme", "Scrabble", "Boggle", "Pictionary", "Balderdash", "Taboo", "Catch Phrase", "Lords of Waterdeep", "Settlers of Catan", "Twister", "Cranium"];
 
 let prices = [12.99, 49.99, 50.00, 100.00, 149.99, 5.95, 25.00, 8.50, 9.89, 88.75, 200.00];
 
 let genres = ["adventure", "RPG", "strategy", "horror", "mystery", "horticulture", "sci-fi", "cooperative", "turn-based", "chance", "card", "simulator", "puzzle", "video", "party", "drinking"];
 
-let times = [5, 10, 15, 20, 25, 30, 45, 60, 90, 120, 150, 180];
+let times = [5, 10, 15, 20, 25, 30, 45, 60, 90, 120, 150, 180, 210, 240, 270, 300];
 
-let designers = ["Mattel", "Hasbro", "Matt Leacock", "Jamey Stegmaier", "Daniel Schewe", "Jared Bourget", "Jeff Bentley", "Teddi Maull", "Ron", "Morgan", "Chase", "Trevor", "Brady", "Han", "Rachel", "Stephen", "Bailey", "Nick", "Jordan", "Sruj", "Cutthroat Craig", "Jacy"];
+let designers = ["Mattel", "Hasbro", "Matt Leacock", "Jamey Stegmaier", "Daniel Schewe", "Jared Bourget", "Jeff Bentley", "Teddi Maull", "Ron", "Morgan", "Chase", "Trevor", "Brady", "Han", "Rachel", "Stephen", "Bailey", "Nick", "Jordan", "Sruj", "Cutthroat Craig", "Jacy", "Bandai", "Grimoire Games"];
 
 const csvWriter = createCsvWriter({
         path: './games.csv',
         header: [
-            {id: 'id', title: 'id'},
+            //{id: 'id', title: 'id'},
             {id: 'name', title: 'name'},
-            {id: 'rank', title: 'rank'},
             {id: 'genre', title: 'genre'},
             {id: 'time', title: 'time'},
             {id: 'price', title: 'price'},
             {id: 'min_players', title: 'min_players'},
             {id: 'max_players', title: 'max_players'},
-            {id: 'designer', title: 'designer'}
+            {id: 'designer', title: 'designer'},
+            {id: 'rank', title: 'rank'},
         ]
     });
 
@@ -30,15 +47,16 @@ let dataGenerator = (array) => {
         array.push({
             // id: i, - this is not needed for Postgres since it will auto-increment
             name: gameNames[Math.ceil(Math.random() * (gameNames.length - 1))],
-            rank: (Math.random() * 5).toFixed(2),
             genre: genres[Math.ceil(Math.random() * (genres.length - 1))],
             time: times[Math.ceil(Math.random() * (times.length - 1))],
-            price: prices[Math.ceil(Math.random() * (prices.legnth - 1))],
-            min_players: Math.ceil(Math.random() * 2),
+            price: prices[Math.ceil(Math.random() * (prices.length - 1))],
+            min_players: Math.ceil(Math.random() * 2 + 1),
             max_players: Math.ceil(Math.random() * 8),
-            designer: designers[Math.ceil(Math.random() * (designers.length - 1))]
+            designer: designers[Math.ceil(Math.random() * (designers.length - 1))],
+            rank: (Math.random() * 5).toFixed(2)
         });
-	}
+    }
+    console.log(array)
 }
 
 // create an array to push data into and then convert it to a csv file
@@ -49,3 +67,6 @@ csvWriter.writeRecords(records)
 .then(() => {
 	console.timeEnd('Processing time: ');
 });
+
+
+// COPY games FROM '/Users/danielschewe/Documents/GHRBLD03/board_game_tracker/database/games.csv' WITH CSV HEADER;
